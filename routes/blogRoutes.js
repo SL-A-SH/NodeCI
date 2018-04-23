be94ef36ce45bc1f6ac24933d23415feae9123b7
Aqeeb <aqeeb.spider@gmail.com>
2018-04-23 22:20:06 +0530

    Travis config

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
+const cleanCache = require('../middlewares/cleanCache');
 
 const Blog = mongoose.model('Blog');
 
@@ -14,12 +15,28 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
-
+    const blogs = await Blog.find({ _user: req.user.id }).cache({key: req.user.id});
     res.send(blogs);
+    // const redis = require('redis');
+    // const redisUrl = 'redis://127.0.0.1:6379';
+    // const client = redis.createClient(redisUrl);
+    // const util = require('util');
+    // client.get = util.promisify(client.get);
+
+    // const cachedBlogs = await client.get(req.user.id);
+
+    // if (cachedBlogs) {
+    //   console.log('serving from cache');
+    //   return res.send(JSON.parse(cachedBlogs));
+    // }
+
+    // console.log('serving from mongodb');
+    // const blogs = await Blog.find({ _user: req.user.id });
+    // res.send(blogs);
+    // client.set(req.user.id, JSON.stringify(blogs));
   });
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post('/api/blogs', requireLogin, cleanCache, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
@@ -34,5 +51,7 @@ module.exports = app => {
     } catch (err) {
       res.send(400, err);
     }
+
+    clearHash(req.user.id);
   });
 };
